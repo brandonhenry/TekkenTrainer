8794f83d0cbfe44b0f7d25764198af21d9fe7e3a
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-03-09 21:00:38 +0100

    GUI initialization first attempt.

--- src/event_buss.zig
+++ src/event_buss.zig
@@ -1,11 +1,13 @@
 const std = @import("std");
 const w32 = @import("win32").everything;
+const gui = @import("zgui");
 const dx12 = @import("dx12/root.zig");
 const misc = @import("misc/root.zig");
 
 pub const EventBuss = struct {
     gpa: std.heap.GeneralPurposeAllocator(.{}),
     dx12_leftovers: ?dx12.Leftovers,
+    is_gui_initialized: bool,
 
     const Self = @This();
 
@@ -15,11 +17,9 @@ pub const EventBuss = struct {
         command_queue: *const w32.ID3D12CommandQueue,
         swap_chain: *const w32.IDXGISwapChain,
     ) Self {
-        _ = window;
-        _ = command_queue;
         _ = swap_chain;
 
-        const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
         std.log.debug("Initializing DX12 leftovers...", .{});
         const dx12_leftovers = if (dx12.Leftovers.init(device)) |leftovers| block: {
@@ -31,9 +31,27 @@ pub const EventBuss = struct {
             break :block null;
         };
 
+        const is_gui_initialized = if (dx12_leftovers) |leftovers| block: {
+            std.log.debug("Initializing GUI...", .{});
+            gui.init(gpa.allocator());
+            gui.backend.init(window, .{
+                .device = device,
+                .command_queue = command_queue,
+                .num_frames_in_flight = 2,
+                .rtv_format = @intFromEnum(w32.DXGI_FORMAT_R8G8B8A8_UNORM),
+                .dsv_format = @intFromEnum(w32.DXGI_FORMAT_UNKNOWN),
+                .cbv_srv_heap = leftovers.descriptor_heap,
+                .font_srv_cpu_desc_handle = @bitCast(leftovers.getCpuDescriptorHandle()),
+                .font_srv_gpu_desc_handle = @bitCast(leftovers.getGpuDescriptorHandle()),
+            });
+            std.log.info("GUI initialized.", .{});
+            break :block true;
+        } else false;
+
         return .{
             .gpa = gpa,
             .dx12_leftovers = dx12_leftovers,
+            .is_gui_initialized = is_gui_initialized,
         };
     }
 
@@ -44,10 +62,19 @@ pub const EventBuss = struct {
         command_queue: *const w32.ID3D12CommandQueue,
         swap_chain: *const w32.IDXGISwapChain,
     ) void {
+        _ = swap_chain;
         _ = window;
         _ = device;
         _ = command_queue;
-        _ = swap_chain;
+
+        std.log.debug("De-initializing GUI...", .{});
+        if (self.is_gui_initialized) {
+            gui.backend.deinit();
+            gui.deinit();
+            std.log.info("GUI de-initialized.", .{});
+        } else {
+            std.log.debug("Nothing to de-initialize.", .{});
+        }
 
         std.log.debug("De-initializing DX12 leftovers...", .{});
         if (self.dx12_leftovers) |leftovers| {
@@ -70,10 +97,30 @@ pub const EventBuss = struct {
         command_queue: *const w32.ID3D12CommandQueue,
         swap_chain: *const w32.IDXGISwapChain,
     ) void {
-        _ = self;
         _ = window;
         _ = device;
         _ = command_queue;
-        _ = swap_chain;
+
+        const leftovers = self.dx12_leftovers orelse return;
+
+        const swap_chain_2: *const w32.IDXGISwapChain2 = @ptrCast(swap_chain);
+        var frame_buffer_width: u32 = undefined;
+        var frame_buffer_height: u32 = undefined;
+        const size_return_code = swap_chain_2.IDXGISwapChain2_GetSourceSize(&frame_buffer_width, &frame_buffer_height);
+        if (size_return_code != w32.S_OK) {
+            misc.errorContext().newFmt(
+                error.Dx12Error,
+                "IDXGISwapChain2.GetSourceSize returned: {}",
+                .{size_return_code},
+            );
+            misc.errorContext().append(error.Dx12Error, "Failed to get frame buffer width and height.");
+            misc.errorContext().logError();
+        }
+
+        gui.backend.newFrame(frame_buffer_width, frame_buffer_height);
+
+        gui.text("Hello World!", .{});
+
+        gui.backend.draw(leftovers.graphics_command_list);
     }
 };
