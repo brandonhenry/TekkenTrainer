8d6ea3d10278aa4d104a0602fbe951e5aab17495
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-05-16 20:06:00 +0200

    Draw data address and offset.

--- src/components/data.zig
+++ src/components/data.zig
@@ -2,15 +2,6 @@ const std = @import("std");
 const imgui = @import("imgui");
 const memory = @import("../memory/root.zig");
 
-const error_color = imgui.ImVec4{ .x = 1, .y = 0.5, .z = 0.5, .w = 1 };
-const null_color = imgui.ImVec4{ .x = 0, .y = 1, .z = 1, .w = 1 };
-
-const Context = struct {
-    label: [:0]const u8,
-    type_name: [:0]const u8,
-    parent: ?*const Context,
-};
-
 pub fn drawData(label: [:0]const u8, pointer: anytype) void {
     const type_name = switch (@typeInfo(@TypeOf(pointer))) {
         .pointer => |info| @typeName(info.child),
@@ -21,6 +12,7 @@ pub fn drawData(label: [:0]const u8, pointer: anytype) void {
     const ctx = Context{
         .label = label,
         .type_name = type_name,
+        .offset = 0,
         .parent = null,
     };
     drawAny(&ctx, pointer);
@@ -44,14 +36,14 @@ fn drawAny(ctx: *const Context, pointer: anytype) void {
         drawSelfSortableArray(ctx, pointer);
     } else switch (@typeInfo(ChildType)) {
         .pointer => drawAny(ctx, pointer.*),
-        .void => drawVoid(ctx),
-        .null => drawNull(ctx),
-        .undefined => drawUndefined(),
+        .void => drawVoid(ctx, pointer),
+        .null => drawNull(ctx, pointer),
+        .undefined => drawUndefined(ctx, pointer),
         .bool => drawBool(ctx, pointer),
-        .int => drawNumber(ctx, pointer),
-        .float => drawNumber(ctx, pointer),
-        .comptime_float => drawComptimeNumber(ctx, pointer),
-        .comptime_int => drawComptimeNumber(ctx, pointer),
+        .int => drawInt(ctx, pointer),
+        .float => drawFloat(ctx, pointer),
+        .comptime_float => drawComptimeInt(ctx, pointer),
+        .comptime_int => drawComptimeFloat(ctx, pointer),
         .@"fn" => drawMemoryAddress(ctx, pointer),
         .@"opaque" => drawMemoryAddress(ctx, pointer),
         .type => drawType(ctx, pointer),
@@ -67,6 +59,8 @@ fn drawAny(ctx: *const Context, pointer: anytype) void {
     }
 }
 
+// Rendering of custom types.
+
 fn drawConvertedValue(ctx: *const Context, pointer: anytype) void {
     drawAny(ctx, &pointer.getValue());
 }
@@ -74,85 +68,202 @@ fn drawConvertedValue(ctx: *const Context, pointer: anytype) void {
 fn drawPointer(ctx: *const Context, pointer: anytype) void {
     if (pointer.toConstPointer()) |ptr| {
         drawAny(ctx, ptr);
-    } else {
-        drawText(ctx.label, "Invalid pointer.", error_color);
+        return;
     }
+
+    const text = "Invalid pointer.";
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawPointerTrail(ctx: *const Context, pointer: anytype) void {
     if (pointer.toConstPointer()) |ptr| {
         drawAny(ctx, ptr);
-    } else {
-        drawText(ctx.label, "Invalid pointer trail.", error_color);
+        return;
     }
+
+    const text = "Invalid pointer trail.";
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawSelfSortableArray(ctx: *const Context, pointer: anytype) void {
     drawAny(ctx, pointer.sortedConst());
 }
 
-fn drawVoid(ctx: *const Context) void {
-    drawText(ctx, "{} (void instance)", null);
+// Rendering of language types.
+
+fn drawVoid(ctx: *const Context, pointer: anytype) void {
+    const text = "{} (void instance)";
+    drawText(ctx, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
-fn drawNull(ctx: *const Context) void {
-    drawText(ctx, "null", null_color);
+fn drawNull(ctx: *const Context, pointer: anytype) void {
+    const text = "null";
+    drawText(ctx, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
-fn drawUndefined(ctx: *const Context) void {
-    drawText(ctx, "undefined", null);
+fn drawUndefined(ctx: *const Context, pointer: anytype) void {
+    const text = "undefined";
+    drawText(ctx, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawBool(ctx: *const Context, pointer: *const bool) void {
     const text = if (pointer.*) "true" else "false";
-    drawText(ctx.label, text, null);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
-fn drawNumber(ctx: *const Context, pointer: anytype) void {
+fn drawInt(ctx: *const Context, pointer: anytype) void {
+    const value = pointer.*;
+    var buffer: [64]u8 = undefined;
+
+    const text = std.fmt.bufPrintZ(&buffer, "{}", .{value}) catch "display error";
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("decimal", text);
+    const hex = std.fmt.bufPrintZ(&buffer, "{X}", .{pointer.*}) catch "display error";
+    drawMenuText("hexadecimal", hex);
+}
+
+fn drawFloat(ctx: *const Context, pointer: anytype) void {
     var buffer: [64]u8 = undefined;
     const text = std.fmt.bufPrintZ(&buffer, "{}", .{pointer.*}) catch "display error";
-    drawText(ctx.label, text, null);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
-fn drawComptimeNumber(ctx: *const Context, pointer: anytype) void {
+fn drawComptimeInt(ctx: *const Context, pointer: *const comptime_int) void {
     const text = std.fmt.comptimePrint("{}", .{pointer.*});
-    drawText(ctx.label, text, null);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
+}
+
+fn drawComptimeFloat(ctx: *const Context, pointer: *const comptime_float) void {
+    const text = std.fmt.comptimePrint("{}", .{pointer.*});
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawMemoryAddress(ctx: *const Context, pointer: anytype) void {
     const address = @intFromPtr(pointer);
+
     var buffer: [64]u8 = undefined;
     const text = std.fmt.bufPrintZ(&buffer, "0x{X}", .{address}) catch "display error";
-    drawText(ctx.label, text, null);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawType(ctx: *const Context, pointer: *const type) void {
     const text = @typeName(pointer.*);
-    drawText(ctx.label, text, null);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawEnumLiteral(ctx: *const Context, pointer: anytype) void {
     const text = @tagName(pointer.*);
-    drawText(ctx.label, text, null);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawEnum(ctx: *const Context, pointer: anytype) void {
-    const ChildType = @typeInfo(@TypeOf(pointer)).pointer.child;
-    const info = @typeInfo(ChildType).@"enum";
+    const info = @typeInfo(@TypeOf(pointer.*)).@"enum";
     const value = @intFromEnum(pointer.*);
+    var text: [:0]const u8 = undefined;
     inline for (info.fields) |*field| {
         if (field.value == value) {
-            drawText(ctx.label, field.name, null);
+            text = field.name;
             break;
         }
     } else {
         drawAny(ctx, &value);
+        return;
     }
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawError(ctx: *const Context, pointer: anytype) void {
     const text = @errorName(pointer.*);
-    drawText(ctx.label, text, error_color);
+    drawText(ctx.label, text);
+
+    if (!beginMenu(ctx, pointer)) return;
+    defer endMenu();
+
+    drawSeparator();
+    drawMenuText("value", text);
 }
 
 fn drawOptional(ctx: *const Context, pointer: anytype) void {
@@ -172,17 +283,20 @@ fn drawErrorUnion(ctx: *const Context, pointer: anytype) void {
 }
 
 fn drawArray(ctx: *const Context, pointer: anytype) void {
-    const ChildType = @typeInfo(@TypeOf(pointer)).pointer.child;
-    const ElementType = @typeInfo(ChildType).array.child;
-    if (!imgui.igTreeNode_Str(ctx.label)) {
-        return;
+    const node_open = beginNode(ctx.label);
+    if (beginMenu(ctx, pointer)) {
+        defer endMenu();
     }
-    defer imgui.igTreePop();
+    if (!node_open) return;
+    defer endNode();
+
+    const ElementType = @typeInfo(@TypeOf(pointer.*)).array.child;
     for (pointer, 0..) |*element_pointer, index| {
         var buffer: [64]u8 = undefined;
         const element_ctx = Context{
             .label = std.fmt.bufPrintZ(&buffer, "{}", .{index}) catch "display error",
             .type_name = @typeName(ElementType),
+            .offset = @intFromPtr(element_pointer) - @intFromPtr(pointer),
             .parent = ctx,
         };
         drawAny(&element_ctx, element_pointer);
@@ -190,40 +304,46 @@ fn drawArray(ctx: *const Context, pointer: anytype) void {
 }
 
 fn drawStruct(ctx: *const Context, pointer: anytype) void {
-    const ChildType = @typeInfo(@TypeOf(pointer)).pointer.child;
-    const info = @typeInfo(ChildType).@"struct";
-    if (!imgui.igTreeNode_Str(ctx.label)) {
-        return;
+    const node_open = beginNode(ctx.label);
+    if (beginMenu(ctx, pointer)) {
+        defer endMenu();
     }
-    defer imgui.igTreePop();
+    if (!node_open) return;
+    defer endNode();
+
+    const info = @typeInfo(@TypeOf(pointer.*)).@"struct";
     inline for (info.fields) |*field| {
         if (comptime std.mem.startsWith(u8, field.name, "_")) {
             continue;
         }
+        const field_pointer = &@field(pointer, field.name);
         const field_ctx = Context{
             .label = field.name,
             .type_name = @typeName(field.type),
+            .offset = @intFromPtr(field_pointer) - @intFromPtr(pointer),
             .parent = ctx,
         };
-        const field_pointer = &@field(pointer, field.name);
         drawAny(&field_ctx, field_pointer);
     }
 }
 
 fn drawUnion(ctx: *const Context, pointer: anytype) void {
-    const ChildType = @typeInfo(@TypeOf(pointer)).pointer.child;
-    const info = @typeInfo(ChildType).@"union";
-    if (!imgui.igTreeNode_Str(ctx.label)) {
-        return;
+    const node_open = beginNode(ctx.label);
+    if (beginMenu(ctx, pointer)) {
+        defer endMenu();
     }
-    defer imgui.igTreePop();
+    if (!node_open) return;
+    defer endNode();
+
+    const info = @typeInfo(@TypeOf(pointer.*)).@"union";
     if (info.tag_type) |tag_type| {
+        const tag_pointer = &@as(tag_type, pointer.*);
         const tag_ctx = Context{
             .label = "tag",
             .type_name = @typeName(tag_type),
+            .offset = @intFromPtr(tag_pointer) - @intFromPtr(pointer),
             .parent = ctx,
         };
-        const tag_pointer = &@as(tag_type, pointer.*);
         drawAny(&tag_ctx, tag_pointer);
 
         const value_pointer = switch (pointer.*) {
@@ -231,20 +351,48 @@ fn drawUnion(ctx: *const Context, pointer: anytype) void {
         };
         const value_ctx = Context{
             .label = "value",
-            .type_name = @typeName(@typeInfo(@TypeOf(value_pointer)).pointer.child),
+            .type_name = @typeName(@TypeOf(value_pointer.*)),
+            .offset = value_pointer - pointer,
             .parent = ctx,
         };
         drawAny(&value_ctx, value_pointer);
     } else inline for (info.fields) |*field| {
+        const field_pointer: *const field.type = @ptrCast(pointer);
         const field_ctx = Context{
             .label = field.name,
             .type_name = @typeName(field.type),
+            .offset = 0,
             .parent = ctx,
         };
-        const field_pointer: *const field.type = @ptrCast(pointer);
         drawAny(&field_ctx, field_pointer);
     }
 }
+
+// Helpers.
+
+const Context = struct {
+    label: [:0]const u8,
+    type_name: [:0]const u8,
+    offset: usize,
+    parent: ?*const Context,
+
+    const Self = @This();
+
+    pub fn getPath(self: *const Self, buffer: []u8) ![:0]u8 {
+        var stream = std.io.fixedBufferStream(buffer);
+        try self.writePath(stream.writer());
+        try stream.writer().writeByte(0);
+        return buffer[0..(stream.pos - 1) :0];
+    }
+
+    fn writePath(self: *const Self, writer: anytype) !void {
+        if (self.parent) |parent| {
+            try parent.writePath(writer);
+            try writer.writeByte('.');
+        }
+        try writer.writeAll(self.label);
+    }
+};
 
 pub inline fn hasTag(comptime Type: type, comptime tag: type) bool {
     comptime {
@@ -255,16 +403,60 @@ pub inline fn hasTag(comptime Type: type, comptime tag: type) bool {
     }
 }
 
-fn drawText(label: [:0]const u8, text: [:0]const u8, color: ?imgui.ImVec4) void {
+fn drawText(label: [:0]const u8, text: [:0]const u8) void {
     imgui.igIndent(0.0);
     defer imgui.igUnindent(0.0);
     imgui.igBeginGroup();
     defer imgui.igEndGroup();
     imgui.igText("%s:", label.ptr);
     imgui.igSameLine(0.0, -0.1);
-    if (color) |col| {
-        imgui.igTextColored(col, "%s", text.ptr);
-    } else {
-        imgui.igText("%s", text.ptr);
-    }
+    imgui.igText("%s", text.ptr);
+}
+
+fn beginNode(label: [:0]const u8) bool {
+    return imgui.igTreeNode_Str(label);
+}
+
+fn endNode() void {
+    imgui.igTreePop();
+}
+
+fn beginMenu(ctx: *const Context, pointer: anytype) bool {
+    imgui.igSetItemTooltip("Right-click to open popup.");
+    const menu_open = imgui.igBeginPopupContextItem(ctx.label, imgui.ImGuiPopupFlags_MouseButtonRight);
+    if (!menu_open) return false;
+
+    var buffer: [128]u8 = undefined;
+
+    drawMenuText("label", ctx.label);
+    drawMenuText("path", ctx.getPath(&buffer) catch "display error");
+    drawMenuText("type", ctx.type_name);
+
+    drawSeparator();
+
+    const address = @intFromPtr(pointer);
+    const address_text = std.fmt.bufPrintZ(&buffer, "0x{X}", .{address}) catch "display error";
+    drawMenuText("address", address_text);
+    const offset = ctx.offset;
+    const offset_text = std.fmt.bufPrintZ(&buffer, "{} (0x{X})", .{ offset, offset }) catch "display error";
+    drawMenuText("offset", offset_text);
+    const size = @sizeOf(@TypeOf(pointer.*));
+    const size_text = std.fmt.bufPrintZ(&buffer, "{} (0x{X})", .{ size, size }) catch "display error";
+    drawMenuText("size", size_text);
+
+    return true;
+}
+
+fn endMenu() void {
+    imgui.igEndPopup();
+}
+
+fn drawMenuText(label: [:0]const u8, text: [:0]const u8) void {
+    imgui.igText("%s:", label.ptr);
+    imgui.igSameLine(0.0, -0.1);
+    imgui.igText("%s", text.ptr);
+}
+
+fn drawSeparator() void {
+    imgui.igSeparator();
 }
