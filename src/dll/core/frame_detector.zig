b78da505fdb3276f4e319434d102aeae03879be5
Tomislav Ivanković <git@tomislav-ivankovic.from.hr>
2025-08-12 15:46:26 +0200

    Don't record when outside game. (In menues.)

--- src/dll/core/frame_detector.zig
+++ src/dll/core/frame_detector.zig
@@ -24,7 +24,9 @@ pub const FrameDetector = struct {
             (player_1.frames_since_round_start == null and
                 player_2.frames_since_round_start == null and
                 player_1.current_move_frame == null and
-                player_2.current_move_frame == null);
+                player_2.current_move_frame == null and
+                !sdk.misc.areAllFieldsNull(player_1) and
+                !sdk.misc.areAllFieldsNull(player_2));
         self.last_player_1.frames_since_round_start = player_1.frames_since_round_start;
         self.last_player_1.current_move_frame = player_1.current_move_frame;
         self.last_player_2.frames_since_round_start = player_2.frames_since_round_start;
@@ -38,29 +40,31 @@ const testing = std.testing;
 test "should detect frames only when frame values are changing or every frame value is null" {
     const detect = struct {
         var frame_detector = FrameDetector{};
-        fn call(frame_1: ?u32, frame_2: ?u32, frame_3: ?u32, frame_4: ?u32) bool {
+        fn call(frame_1: ?u32, frame_2: ?u32, frame_3: ?u32, frame_4: ?u32, id_1: ?u32, id_2: ?u32) bool {
             return frame_detector.detect(
-                &.{ .frames_since_round_start = frame_1, .current_move_frame = frame_2 },
-                &.{ .frames_since_round_start = frame_3, .current_move_frame = frame_4 },
+                &.{ .frames_since_round_start = frame_1, .current_move_frame = frame_2, .character_id = id_1 },
+                &.{ .frames_since_round_start = frame_3, .current_move_frame = frame_4, .character_id = id_2 },
             );
         }
     }.call;
-    try testing.expectEqual(true, detect(null, null, null, null));
-    try testing.expectEqual(true, detect(null, null, null, null));
-    try testing.expectEqual(true, detect(1, null, null, null));
-    try testing.expectEqual(false, detect(1, null, null, null));
-    try testing.expectEqual(true, detect(2, null, null, null));
-    try testing.expectEqual(false, detect(2, null, null, null));
-    try testing.expectEqual(true, detect(2, 1, null, null));
-    try testing.expectEqual(false, detect(2, 1, null, null));
-    try testing.expectEqual(true, detect(2, 2, null, null));
-    try testing.expectEqual(false, detect(2, 2, null, null));
-    try testing.expectEqual(true, detect(2, 2, 1, null));
-    try testing.expectEqual(false, detect(2, 2, 1, null));
-    try testing.expectEqual(true, detect(2, 2, 2, null));
-    try testing.expectEqual(false, detect(2, 2, 2, null));
-    try testing.expectEqual(true, detect(2, 2, 2, 1));
-    try testing.expectEqual(false, detect(2, 2, 2, 1));
-    try testing.expectEqual(true, detect(2, 2, 2, 2));
-    try testing.expectEqual(false, detect(2, 2, 2, 2));
+    try testing.expectEqual(false, detect(null, null, null, null, null, null));
+    try testing.expectEqual(false, detect(null, null, null, null, null, null));
+    try testing.expectEqual(true, detect(null, null, null, null, 1, 2));
+    try testing.expectEqual(true, detect(null, null, null, null, 1, 2));
+    try testing.expectEqual(true, detect(1, null, null, null, null, null));
+    try testing.expectEqual(false, detect(1, null, null, null, null, null));
+    try testing.expectEqual(true, detect(2, null, null, null, null, null));
+    try testing.expectEqual(false, detect(2, null, null, null, null, null));
+    try testing.expectEqual(true, detect(2, 1, null, null, null, null));
+    try testing.expectEqual(false, detect(2, 1, null, null, null, null));
+    try testing.expectEqual(true, detect(2, 2, null, null, null, null));
+    try testing.expectEqual(false, detect(2, 2, null, null, null, null));
+    try testing.expectEqual(true, detect(2, 2, 1, null, null, null));
+    try testing.expectEqual(false, detect(2, 2, 1, null, null, null));
+    try testing.expectEqual(true, detect(2, 2, 2, null, null, null));
+    try testing.expectEqual(false, detect(2, 2, 2, null, null, null));
+    try testing.expectEqual(true, detect(2, 2, 2, 1, null, null));
+    try testing.expectEqual(false, detect(2, 2, 2, 1, null, null));
+    try testing.expectEqual(true, detect(2, 2, 2, 2, null, null));
+    try testing.expectEqual(false, detect(2, 2, 2, 2, null, null));
 }
--- src/sdk/misc/meta.zig
+++ src/sdk/misc/meta.zig
@@ -52,6 +52,29 @@ pub fn FieldMap(comptime KeysStruct: type, comptime Value: type) type {
     return @Type(.{ .@"struct" = map_struct });
 }
 
+pub fn areAllFieldsNull(struct_pointer: anytype) bool {
+    const Struct = switch (@typeInfo(@TypeOf(struct_pointer))) {
+        .pointer => |pointer| pointer.child,
+        else => @compileError(
+            "The areAllFieldsNull function expects a pointer to struct but provided value is of type: " ++
+                @typeName(@TypeOf(struct_pointer)),
+        ),
+    };
+    const fields: []const std.builtin.Type.StructField = switch (@typeInfo(Struct)) {
+        .@"struct" => |info| info.fields,
+        else => @compileError(
+            "The areAllFieldsNull function expects a pointer to struct but provided value is of type: " ++
+                @typeName(@TypeOf(struct_pointer)),
+        ),
+    };
+    inline for (fields) |*field| {
+        if (@field(struct_pointer, field.name) != null) {
+            return false;
+        }
+    }
+    return true;
+}
+
 const testing = std.testing;
 
 test "FieldMap should make every field typed with the specified type" {
@@ -88,4 +111,24 @@ test "Partial should make every field's default value null" {
     try testing.expectEqual(null, partial.field_1);
     try testing.expectEqual(null, partial.field_2);
     try testing.expectEqual(null, partial.field_3);
+}
+
+test "areAllFieldsNull should return null only if all fields of provided struct are null" {
+    const Struct = struct {
+        field_1: ?u8,
+        field_2: ?u16,
+        field_3: ?u32,
+    };
+    const struct_1 = Struct{
+        .field_1 = null,
+        .field_2 = null,
+        .field_3 = null,
+    };
+    const struct_2 = Struct{
+        .field_1 = null,
+        .field_2 = 0,
+        .field_3 = null,
+    };
+    try testing.expectEqual(true, areAllFieldsNull(&struct_1));
+    try testing.expectEqual(false, areAllFieldsNull(&struct_2));
 }
